a0d72820b78f1475080e03080f1bce0cac96af46
SleytherG <sleyther.calsin@cloudcomputing.com.pe>
2022-06-12 07:07:47 -0500

    First Commit

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class HomePageTemp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp')
+      ),
+      body: ListView(
+        children: const <Widget>[
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title'),
+          ),
+        ]
+      )
+    );
+  }
+}
